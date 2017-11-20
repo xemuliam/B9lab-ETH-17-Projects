891256306e7948558c037fa23d385bd132f55e4c
Michael Whittle <michael@lifecycle-ps.com>
2017-11-20 15:40:58 +0100

    cleaning up after getOwner change

--- Module_4-Splitter/test/splitter.js
+++ Module_4-Splitter/test/splitter.js
@@ -19,7 +19,7 @@ contract('Contract', function(accounts) {
   });
 
   it("should be owned by the owner", function() {
-    return myContract.owner( { from:owner } )
+    return myContract.getOwner( { from:owner } )
     .then( _owner => {
         assert.strictEqual( _owner, owner, "Contract is not owned by owner");
     });
