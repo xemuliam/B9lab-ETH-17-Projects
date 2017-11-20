16cf9da1af53778aaacd2c295cbfd6638b31aed8
Michael Whittle <michael@lifecycle-ps.com>
2017-11-20 16:28:39 +0100

    fixed the 'callback hell' in the unit tests

--- Module_4-RockPaperScissors/test/rockpaperscissors.js
+++ Module_4-RockPaperScissors/test/rockpaperscissors.js
@@ -43,133 +43,133 @@ contract('Contract', function(accounts) {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
       assert.equal( txDeposit.logs[0].args.deposit.toString(10), correctDepositAmount, "player one registered with the incorrect deposit amount" );
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
         assert.equal( txDeposit.logs[0].args.deposit.toString(10), correctDepositAmount, "player two registered with the incorrect deposit amount" );
       });
-    });
   });
 
   it("should have player one and two draw with Rock", function() {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
-        return myContract.getChoiceBytes32( Choices.Rock, { from:playerOne } )
-        .then( receivedValue => {
-          return myContract.playerChoice( receivedValue, { from:playerOne } )
-          .then( receivedValue => {
-            return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } )
-            .then( receivedValue => {  
-              return myContract.playerChoice( receivedValue, { from:playerTwo } )
-              .then( receivedValue => {
-                assert.equal( receivedValue.logs[1].args.result, 0, "should have been a draw" );
-              });
-            });
-          });
-        });
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } );
+      })
+      .then( receivedValue => {  
+        return myContract.playerChoice( receivedValue, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        assert.equal( receivedValue.logs[1].args.result, 0, "should have been a draw" );
       });
-    });
   });
 
   it("should have player one win with Paper and player two with Rock", function() {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
-        return myContract.getChoiceBytes32( Choices.Paper, { from:playerOne } )
-        .then( receivedValue => {
-          return myContract.playerChoice( receivedValue, { from:playerOne } )
-          .then( receivedValue => {
-            return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } )
-            .then( receivedValue => {
-              return myContract.playerChoice( receivedValue, { from:playerTwo } )
-              .then( receivedValue => {
-                assert.equal( receivedValue.logs[1].args.result, 1, "player one should have won" );
-              });
-            });
-          });
-        });
+        return myContract.getChoiceBytes32( Choices.Paper, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        assert.equal( receivedValue.logs[1].args.result, 1, "player one should have won" );
       });
-    });
   });
 
   it("should have player two win with Rock and player one with Scissors", function() {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
-        return myContract.getChoiceBytes32( Choices.Scissors, { from:playerOne } )
-        .then( receivedValue => {
-          return myContract.playerChoice( receivedValue, { from:playerOne } )
-          .then( receivedValue => {
-            return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } )
-            .then( receivedValue => {
-              return myContract.playerChoice( receivedValue, { from:playerTwo } )
-              .then( receivedValue => {
-                assert.equal( receivedValue.logs[1].args.result, 2, "player two should have won" );
-              });
-            });
-          });
-        });
+        return myContract.getChoiceBytes32( Choices.Scissors, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } )
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerTwo } )
+      })
+      .then( receivedValue => {
+        assert.equal( receivedValue.logs[1].args.result, 2, "player two should have won" );
       });
-    });
   });
 
   it("should have player one withdraw their winnings", function() {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
-        return myContract.getChoiceBytes32( Choices.Paper, { from:playerOne } )
-        .then( receivedValue => {
-          return myContract.playerChoice( receivedValue, { from:playerOne } )
-          .then( receivedValue => {
-            return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } )
-            .then( receivedValue => {
-              return myContract.playerChoice( receivedValue, { from:playerTwo } )
-              .then( receivedValue => {
-                return myContract.withdraw( { from:playerOne } )
-                .then( receivedValue => {
-                  return myContract.recipientStructs( playerOne, { from:owner } )
-                  .then ( receivedValue => {
-                    assert.equal( receivedValue[0].toString(10), 0, "The player one should have a nil balance after withdrawl" );
-                  });
-                });               
-              });
-            });
-          });
-        });
+        return myContract.getChoiceBytes32( Choices.Paper, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.withdraw( { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.recipientStructs( playerOne, { from:owner } );
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), 0, "The player one should have a nil balance after withdrawl" );
       });
-    });
   });
 
   it("should have player two withdraw their winnings", function() {
     return myContract.register( { from:playerOne, value:correctDepositAmount } )
     .then( function(txDeposit) {
-      return myContract.register( { from:playerTwo, value:correctDepositAmount } )
+      return myContract.register( { from:playerTwo, value:correctDepositAmount } );
+      })
       .then( function(txDeposit) {
-        return myContract.getChoiceBytes32( Choices.Rock, { from:playerOne } )
-        .then( receivedValue => {
-          return myContract.playerChoice( receivedValue, { from:playerOne } )
-          .then( receivedValue => {
-            return myContract.getChoiceBytes32( Choices.Paper, { from:playerTwo } )
-            .then( receivedValue => {
-              return myContract.playerChoice( receivedValue, { from:playerTwo } )
-              .then( receivedValue => {
-                return myContract.withdraw( { from:playerTwo } )
-                .then( receivedValue => {
-                  return myContract.recipientStructs( playerTwo, { from:owner } )
-                  .then ( receivedValue => {
-                    assert.equal( receivedValue[0].toString(10), 0, "The player two should have a nil balance after withdrawl" );
-                  });
-                });
-              });
-            });
-          });
-        });
+        return myContract.getChoiceBytes32( Choices.Rock, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerOne } );
+      })
+      .then( receivedValue => {
+        return myContract.getChoiceBytes32( Choices.Paper, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.playerChoice( receivedValue, { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.withdraw( { from:playerTwo } );
+      })
+      .then( receivedValue => {
+        return myContract.recipientStructs( playerTwo, { from:owner } )
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), 0, "The player two should have a nil balance after withdrawl" );
       });
-    });
   });
 
   it("should have a contract that is not paused on creation", function() {
@@ -182,24 +182,24 @@ contract('Contract', function(accounts) {
   it("should have a contract that is paused after being paused", function() {
     return myContract.pauseContract( { from:owner } )
     .then( receivedValue => {
-      return myContract.isPaused( { from:owner } )
+      return myContract.isPaused( { from:owner } );
+      })
       .then( receivedValue => {
         assert.isOk( receivedValue, "The contract is not paused after being paused" );
       });
-    });
   });
 
   it("should have a contract that is resumed after being paused", function() {
     return myContract.pauseContract( { from:owner } )
     .then( receivedValue => {
-      return myContract.startContract( { from:owner } )
+      return myContract.startContract( { from:owner } );
+      })
       .then( receivedValue => {
-        return myContract.isPaused( { from:owner } )
-        .then( receivedValue => {
-          assert.isNotOk( receivedValue, "The contract is paused after being resumed" );
-        });
+        return myContract.isPaused( { from:owner } );
+      })
+      .then( receivedValue => {
+        assert.isNotOk( receivedValue, "The contract is paused after being resumed" );
       });
-    });
   });
 
 });
--- Module_4-Splitter/test/splitter.js
+++ Module_4-Splitter/test/splitter.js
@@ -31,13 +31,13 @@ contract('Contract', function(accounts) {
       assert.equal( receivedValue.logs.length, 1, "Registration of the first recipient was not successful" );
       assert.strictEqual( receivedValue.logs[0].args.recipient, recipientOne, "Registration of the first recipient was not successful" );
       assert.isOk( receivedValue , "Registration of the first recipient was not successful" );
-      return myContract.registerRecipient( recipientTwo, { from:owner } )
+      return myContract.registerRecipient( recipientTwo, { from:owner } );
+      })
       .then( receivedValue => {
         assert.equal( receivedValue.logs.length, 1, "Registration of the second recipient was not successful" );
         assert.strictEqual( receivedValue.logs[0].args.recipient, recipientTwo, "Registration of the second recipient was not successful" );
         assert.isOk( receivedValue , "Registration of the second recipient was not successful" );
       });
-    });
   });
 
   it("should have a nil balance for the first recipient after registration", function() {
@@ -53,31 +53,31 @@ contract('Contract', function(accounts) {
   it("should have a nil balance for the second recipient after registration", function() {
     return myContract.registerRecipient( recipientTwo, { from:owner } )
     .then( receivedValue => {
-      return myContract.recipientStructs( recipientTwo, { from:owner } )
+      return myContract.recipientStructs( recipientTwo, { from:owner } );
+      })
       .then ( receivedValue => {
         assert.equal( receivedValue[0].toString(10), 0, "The second recipient does not have a nil balance on registration" );
       });
-    });
   });
 
   it("should split the transfer amount from the owner between the two recipients", function() {
     return myContract.registerRecipient( recipientOne, { from:owner } )
     .then( receivedValue => {
-      return myContract.registerRecipient( recipientTwo, { from:owner } )
+      return myContract.registerRecipient( recipientTwo, { from:owner } );
+      })
       .then( receivedValue => {
-        return myContract.deposit( { from:owner, value: amountToSplit } )
-        .then( function(txSplit) {
-          return myContract.recipientStructs( recipientOne, { from:owner } )
-          .then ( receivedValue => {
-            assert.equal( receivedValue[0].toString(10), expectedShare, "The first recipient has the incorrect share" );
-            return myContract.recipientStructs( recipientTwo, { from:owner } )
-            .then ( receivedValue => {
-              assert.equal( receivedValue[0].toString(10), expectedShare, "The second recipient has the incorrect share" );
-            });
-          });
-        });
+        return myContract.deposit( { from:owner, value: amountToSplit } );
+      })
+      .then( function(txSplit) {
+        return myContract.recipientStructs( recipientOne, { from:owner } );
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), expectedShare, "The first recipient has the incorrect share" );
+        return myContract.recipientStructs( recipientTwo, { from:owner } );
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), expectedShare, "The second recipient has the incorrect share" );
       });
-    });
   });
 
   it("should have a contract that is not paused on creation", function() {
@@ -90,47 +90,47 @@ contract('Contract', function(accounts) {
   it("should have a contract that is paused after being paused", function() {
     return myContract.pauseContract( { from:owner } )
     .then( receivedValue => {
-      return myContract.isPaused( { from:owner } )
+      return myContract.isPaused( { from:owner } );
+      })
       .then( receivedValue => {
         assert.isOk( receivedValue, "The contract is not paused after being paused" );
       });
-    });
   });
 
   it("should have a contract that is resumed after being paused", function() {
     return myContract.pauseContract( { from:owner } )
-    .then( receivedValue => {
-      return myContract.startContract( { from:owner } )
       .then( receivedValue => {
-        return myContract.isPaused( { from:owner } )
-        .then( receivedValue => {
-          assert.isNotOk( receivedValue, "The contract is paused after being resumed" );
-        });
+        return myContract.startContract( { from:owner } );
+      })
+      .then( receivedValue => {
+        return myContract.isPaused( { from:owner } );
+      })
+      .then( receivedValue => {
+        assert.isNotOk( receivedValue, "The contract is paused after being resumed" );
       });
-    });
   });
 
   it("should allow a recipient to withdraw their funds", function() {
     return myContract.registerRecipient( recipientOne, { from:owner } )
-    .then( receivedValue => {
-      return myContract.registerRecipient( recipientTwo, { from:owner } )
       .then( receivedValue => {
-        return myContract.deposit( { from:owner, value: amountToSplit } )
-        .then( function(txSplit) {
-          return myContract.recipientStructs( recipientOne, { from:owner } )
-          .then ( receivedValue => {
-            assert.equal( receivedValue[0].toString(10), expectedShare, "The first recipient has the incorrect share" );
-            return myContract.withdraw( { from:recipientOne } )
-            .then( receivedValue => {
-              return myContract.recipientStructs( recipientOne, { from:owner } )
-              .then ( receivedValue => {
-                assert.equal( receivedValue[0].toString(10), 0, "The first recipient should have a nil balance after withdrawl" );
-              });
-            });
-          });
-        });
+        return myContract.registerRecipient( recipientTwo, { from:owner } );
+      })
+      .then( receivedValue => {
+        return myContract.deposit( { from:owner, value: amountToSplit } );
+      })
+      .then( function(txSplit) {
+        return myContract.recipientStructs( recipientOne, { from:owner } );
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), expectedShare, "The first recipient has the incorrect share" );
+        return myContract.withdraw( { from:recipientOne } );
+      })
+      .then( receivedValue => {
+        return myContract.recipientStructs( recipientOne, { from:owner } );
+      })
+      .then ( receivedValue => {
+        assert.equal( receivedValue[0].toString(10), 0, "The first recipient should have a nil balance after withdrawl" );
       });
-    });
   });
 
 });
