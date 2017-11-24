64c1cc92f97e4d345e6ad4a0f5afcd8df8f5bca8
Michael Whittle <michael@lifecycle-ps.com>
2017-11-24 16:53:03 +0100

    added logging and campaign count to Hub

--- Module_4-RockPaperScissors/contracts/Hub.sol
+++ Module_4-RockPaperScissors/contracts/Hub.sol
@@ -7,13 +7,24 @@ contract Hub is Pausable {
   
   address[] public campaigns;
 
+  event LogNewCampaign(address campaign, uint depositAmount);
+
   function newCampaign(uint depositAmount)
     public
     returns(address contractAddress)
   {
     RockPaperScissors trustedCampaign = new RockPaperScissors(depositAmount);
     campaigns.push(trustedCampaign);
+    LogNewCampaign(trustedCampaign, depositAmount);
     return trustedCampaign;
+  }
+
+  function getCampaignCount()
+    public
+    constant
+    returns(uint campaignCount)
+  {
+    return campaigns.length;
   }
 
 }
