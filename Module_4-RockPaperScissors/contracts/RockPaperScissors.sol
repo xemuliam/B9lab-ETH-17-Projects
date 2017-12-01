74ca8068a20bb444b299e12f271291d49916991d
Michael Whittle <michael@lifecycle-ps.com>
2017-12-01 09:54:21 +0000

    major update from feedback

--- Module_4-RockPaperScissors/contracts/RockPaperScissors.sol
+++ Module_4-RockPaperScissors/contracts/RockPaperScissors.sol
@@ -4,7 +4,8 @@ import '../contractLibrary/contracts/contractModifiers/Pausable.sol';
 
 contract RockPaperScissors is Pausable {
 
-    enum Choices { Rock, Paper, Scissors }
+    enum Choices { None, Rock, Paper, Scissors }
+    enum Outcomes { Draw, PlayerOneWinner, PlayerTwoWinner }
 
     struct PlayerStruct {
         uint        balance;
@@ -17,19 +18,19 @@ contract RockPaperScissors is Pausable {
         address     playerTwo;
         bytes32     playerTwoChoice;
         uint        gameStake;
-        uint        gameWinner;
+        Outcomes    gameWinner;
     }
 
     uint public gameNumber;
     uint public correctDepositAmount;
     
     mapping(uint => GameStruct) private gameStructs;
-    mapping(bytes32 => mapping(bytes32 => uint)) private playerChoices;
+    mapping(bytes32 => mapping(bytes32 => Outcomes)) public playerOptions;
     mapping(address => PlayerStruct) private playerBalances;
     
-    event LogDeposit(address sender, uint deposit, uint balance);
-    event LogPlayer(address sender, uint gameNumber, uint playerNumber, Choices choice);
-    event LogOutcome(uint gameNumber, uint gameResult);
+    event LogDeposit(address sender, uint deposit);
+    event LogPlayer(address sender, uint gameNumber, uint playerNumber, bytes32 hashChoice);
+    event LogOutcome(uint gameNumber, Outcomes gameResult);
     event LogWithdraw(address sender, uint amount);
     
     modifier gameNotInProgress() {
@@ -42,18 +43,18 @@ contract RockPaperScissors is Pausable {
     {
         require(setDepositAmount > 0);
         
-        gameNumber = 1;
+        gameNumber = 0;
         correctDepositAmount = setDepositAmount;
         
-        playerChoices[keccak256(Choices.Rock)][keccak256(Choices.Rock)] = 0;
-        playerChoices[keccak256(Choices.Rock)][keccak256(Choices.Paper)] = 2;
-        playerChoices[keccak256(Choices.Rock)][keccak256(Choices.Scissors)] = 1;
-        playerChoices[keccak256(Choices.Paper)][keccak256(Choices.Paper)] = 0;
-        playerChoices[keccak256(Choices.Paper)][keccak256(Choices.Rock)] = 1;
-        playerChoices[keccak256(Choices.Paper)][keccak256(Choices.Scissors)] = 2;
-        playerChoices[keccak256(Choices.Scissors)][keccak256(Choices.Scissors)] = 0;
-        playerChoices[keccak256(Choices.Scissors)][keccak256(Choices.Rock)] = 2;
-        playerChoices[keccak256(Choices.Scissors)][keccak256(Choices.Paper)] = 1;
+        playerOptions[keccak256(Choices.Rock)][keccak256(Choices.Rock)] = Outcomes.Draw;
+        playerOptions[keccak256(Choices.Rock)][keccak256(Choices.Paper)] = Outcomes.PlayerTwoWinner;
+        playerOptions[keccak256(Choices.Rock)][keccak256(Choices.Scissors)] = Outcomes.PlayerOneWinner;
+        playerOptions[keccak256(Choices.Paper)][keccak256(Choices.Paper)] = Outcomes.Draw;
+        playerOptions[keccak256(Choices.Paper)][keccak256(Choices.Rock)] = Outcomes.PlayerOneWinner;
+        playerOptions[keccak256(Choices.Paper)][keccak256(Choices.Scissors)] = Outcomes.PlayerTwoWinner;
+        playerOptions[keccak256(Choices.Scissors)][keccak256(Choices.Scissors)] = Outcomes.Draw;
+        playerOptions[keccak256(Choices.Scissors)][keccak256(Choices.Rock)] = Outcomes.PlayerTwoWinner;
+        playerOptions[keccak256(Choices.Scissors)][keccak256(Choices.Paper)] = Outcomes.PlayerOneWinner;
     }
     
     function playerChoice(Choices choice)
@@ -64,35 +65,37 @@ contract RockPaperScissors is Pausable {
     {
         require(msg.value == correctDepositAmount);
         playerBalances[msg.sender].balance += msg.value;
-        LogDeposit(msg.sender, msg.value, playerBalances[msg.sender].balance);
-        gameStructs[gameNumber].gameStake = correctDepositAmount;
+        LogDeposit(msg.sender, msg.value);
+
+        GameStruct storage game = gameStructs[gameNumber];
+        game.gameStake = correctDepositAmount;
         
-        if(gameStructs[gameNumber].playerOne == 0) {
+        if(game.playerOne == 0) {
             playerBalances[msg.sender].isInGame = true;
-            gameStructs[gameNumber].playerOne = msg.sender;
-            gameStructs[gameNumber].playerOneChoice = keccak256(choice);
-            LogPlayer(msg.sender, gameNumber, 1, choice);
+            game.playerOne = msg.sender;
+            game.playerOneChoice = keccak256(choice);
+            LogPlayer(msg.sender, gameNumber, 1, game.playerOneChoice);
             
-        } else if(gameStructs[gameNumber].playerTwo == 0) {
-            require(msg.sender != gameStructs[gameNumber].playerOne);
+        } else if(game.playerTwo == 0) {
+            require(msg.sender != game.playerOne);
             
-            gameStructs[gameNumber].playerTwo = msg.sender;
-            gameStructs[gameNumber].playerTwoChoice = keccak256(choice);
-            LogPlayer(msg.sender, gameNumber, 2, choice);
+            game.playerTwo = msg.sender;
+            game.playerTwoChoice = keccak256(choice);
+            LogPlayer(msg.sender, gameNumber, 2, game.playerTwoChoice);
             
-            uint gameResult = playerChoices[gameStructs[gameNumber].playerOneChoice][gameStructs[gameNumber].playerTwoChoice];
-            gameStructs[gameNumber].gameWinner = gameResult;
+            Outcomes gameResult = playerOptions[game.playerOneChoice][game.playerTwoChoice];
+            game.gameWinner = gameResult;
             LogOutcome(gameNumber, gameResult);
             
-            if(gameResult == 1) {
-                playerBalances[gameStructs[gameNumber].playerOne].balance += gameStructs[gameNumber].gameStake;
-                playerBalances[gameStructs[gameNumber].playerTwo].balance -= gameStructs[gameNumber].gameStake;
-            } else if(gameResult == 2) {
-                playerBalances[gameStructs[gameNumber].playerOne].balance -= gameStructs[gameNumber].gameStake;
-                playerBalances[gameStructs[gameNumber].playerTwo].balance += gameStructs[gameNumber].gameStake;
+            if(gameResult == Outcomes.PlayerOneWinner) {
+                playerBalances[game.playerOne].balance += game.gameStake;
+                playerBalances[game.playerTwo].balance -= game.gameStake;
+            } else if(gameResult == Outcomes.PlayerTwoWinner) {
+                playerBalances[game.playerOne].balance -= game.gameStake;
+                playerBalances[game.playerTwo].balance += game.gameStake;
             }
             
-            playerBalances[gameStructs[gameNumber].playerOne].isInGame = false;
+            playerBalances[game.playerOne].isInGame = false;
             gameNumber++;
         }
         
@@ -116,12 +119,38 @@ contract RockPaperScissors is Pausable {
         return true;
     }
     
+    /** Getters */
+
     function getBalance()
         public
         view
         returns(uint balance)
     {
         return playerBalances[msg.sender].balance;
+    }
+
+    function getGameStruct(uint index) 
+        public
+        view
+        returns(GameStruct gameStruct)
+    {
+        return gameStructs[index];
+    }
+
+    function getPlayerBalance(address player)
+        public
+        view
+        returns(uint balance)
+    {
+        return playerBalances[player].balance;
+    }
+
+    function isPlayerInGame(address player)
+        public
+        view
+        returns(bool isIndeed)
+    {
+        return playerBalances[player].isInGame;
     }
     
 }
--- Module_4-RockPaperScissors/test/rockpaperscissors.js
+++ Module_4-RockPaperScissors/test/rockpaperscissors.js
@@ -2,95 +2,123 @@ var RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
 
 contract('Contract', function(accounts) {
 
-  const Choices = {
-    Rock: 0,
-    Paper: 1,
-    Scissors: 2 
-  };
+    const Choices = {
+        None: 0,
+        Rock: 1,
+        Paper: 2,
+        Scissors: 3
+    };
 
-  var myContract;
+    var myContract;
 
-  const owner = accounts[0]; 
-  const playerOne = accounts[1]; 
-  const playerTwo = accounts[2]; 
-  const correctDepositAmount = 100000000000000000;
+    const owner = accounts[0]; 
+    const playerOne = accounts[1]; 
+    const playerTwo = accounts[2]; 
+    const correctDepositAmount = 100000000000000000;
 
-  describe("RockPaperScissors", function() {
+    describe("RockPaperScissors", function() {
 
-    beforeEach( function() {
-      return RockPaperScissors.new( correctDepositAmount, { from : owner } )
-      .then( function( instance ) {
-        myContract = instance;
-      });
-    });
-
-    it("should be owned by the owner", function() {
-      return myContract.getOwner( { from:owner } )
-      .then( receivedValue => {
-        assert.strictEqual( receivedValue, owner, "Contract is not owned by owner");
-      });
-    });
-
-    it("should have the correct deposit amount set after creation", function() {
-      return myContract.correctDepositAmount( { from:owner } )
-      .then( receivedValue => {
-        assert.equal( receivedValue.toString(10), correctDepositAmount, "constructor deposit amount not set" );
-      });
-    });
-
-    it("should have a positive result with winning choices", function() {
-
-      [
-        [ Choices.Rock, Choices.Scissors, 1 ],
-        //[ Choices.Rock, Choices.Scissors, 1 ],
-        //[ Choices.Paper, Choices.Rock, 1 ],
-        //[ Choices.Scissors, Choices.Paper, 1 ],
-      ].forEach( winningChoices => {
-
-        return myContract.playerChoice( winningChoices[0], { from:playerOne, value:correctDepositAmount } )
-        .then( receivedValue => {
-          return myContract.playerChoice( winningChoices[1], { from:playerTwo, value:correctDepositAmount } );
-        })
-        .then( receivedValue => {  
-          assert.equal( receivedValue.logs[2].args.gameResult, winningChoices[2], "unexpected player one" );
+        beforeEach( function() {
+            return RockPaperScissors.new( correctDepositAmount, { from : owner } )
+                .then( function( instance ) {
+                    myContract = instance;
+                });
         });
 
-        // TODO: find out how to wait for the promises to come back before looping.
+        it("should be owned by the owner", function() {
+            return myContract.getOwner( { from:owner } )
+                .then( receivedValue => {
+                    assert.strictEqual( receivedValue, owner, "Contract is not owned by owner");
+                });
+        });
 
-      });
+        it("should have the correct deposit amount set after creation", function() {
+            return myContract.correctDepositAmount( { from:owner } )
+                .then( receivedValue => {
+                    assert.equal( receivedValue.toString(10), correctDepositAmount, "constructor deposit amount not set" );
+                });
+        });
+
+        [
+            [ Choices.Rock, Choices.Scissors, 1 ],
+            [ Choices.Paper, Choices.Rock, 1 ],
+            [ Choices.Scissors, Choices.Paper, 1 ],
+        ].forEach( winningChoices => {
+            let playerOneChoice, playerTwoChoice, expectedWinner;
+
+            playerOneChoice = winningChoices[0];
+            playerTwoChoice = winningChoices[1];
+            expectedWinner = winningChoices[2];
+
+            it("player one: " + winningChoices[0] + "; " + "player two: " + winningChoices[1] + "; expected winner: " + winningChoices[2], function() {
+
+                return myContract.playerChoice( playerOneChoice, { from:playerOne, value:correctDepositAmount } )
+                    .then( receivedValue => {
+                        return myContract.playerChoice( playerTwoChoice, { from:playerTwo, value:correctDepositAmount } );
+                    })
+                    .then( receivedValue => {  
+                        assert.equal( receivedValue.logs[2].args.gameResult, expectedWinner, "unexpected winner" );
+                    });
+
+            });
+
+        });
+
+        [
+            [ Choices.Rock, Choices.Scissors, 2 ],
+            [ Choices.Paper, Choices.Rock, 2 ],
+            [ Choices.Scissors, Choices.Paper, 2 ],
+        ].forEach( winningChoices => {
+            let playerOneChoice, playerTwoChoice, expectedLoser;
+
+            playerOneChoice = winningChoices[0];
+            playerTwoChoice = winningChoices[1];
+            expectedLoser = winningChoices[2];
+
+            it("player one: " + winningChoices[0] + "; " + "player two: " + winningChoices[1] + "; expected loser: " + winningChoices[2], function() {
+
+                return myContract.playerChoice( playerOneChoice, { from:playerOne, value:correctDepositAmount } )
+                    .then( receivedValue => {
+                        return myContract.playerChoice( playerTwoChoice, { from:playerTwo, value:correctDepositAmount } );
+                    })
+                    .then( receivedValue => {  
+                        assert.notEqual( receivedValue.logs[2].args.gameResult, expectedLoser, "unexpected loser" );
+                    });
+
+            });
+
+        });
+
+        it("should have a contract that is not paused on creation", function() {
+            return myContract.isPaused( { from:owner } )
+                .then( receivedValue => {
+                    assert.isFalse( receivedValue, "The contract is paused on creation" );
+                });
+        });
+
+        it("should have a contract that is paused after being paused", function() {
+            return myContract.pauseContract( { from:owner } )
+                .then( receivedValue => {
+                    return myContract.isPaused( { from:owner } );
+                })
+                .then( receivedValue => {
+                    assert.isTrue( receivedValue, "The contract is not paused after being paused" );
+                });
+        });
+
+        it("should have a contract that is resumed after being paused", function() {
+            return myContract.pauseContract( { from:owner } )
+                .then( receivedValue => {
+                    return myContract.startContract( { from:owner } );
+                })
+                .then( receivedValue => {
+                    return myContract.isPaused( { from:owner } );
+                })
+                .then( receivedValue => {
+                    assert.isFalse( receivedValue, "The contract is paused after being resumed" );
+                });
+        });
 
     });
-
-    it("should have a contract that is not paused on creation", function() {
-      return myContract.isPaused( { from:owner } )
-      .then( receivedValue => {
-        assert.isNotOk( receivedValue, "The contract is paused on creation" );
-      });
-    });
-
-    it("should have a contract that is paused after being paused", function() {
-      return myContract.pauseContract( { from:owner } )
-      .then( receivedValue => {
-        return myContract.isPaused( { from:owner } );
-      })
-      .then( receivedValue => {
-        assert.isOk( receivedValue, "The contract is not paused after being paused" );
-      });
-   });
-
-    it("should have a contract that is resumed after being paused", function() {
-      return myContract.pauseContract( { from:owner } )
-      .then( receivedValue => {
-        return myContract.startContract( { from:owner } );
-      })
-      .then( receivedValue => {
-        return myContract.isPaused( { from:owner } );
-      })
-      .then( receivedValue => {
-        assert.isNotOk( receivedValue, "The contract is paused after being resumed" );
-      });
-    });
-
-  });
 
 });
